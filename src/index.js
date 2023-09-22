e23770f9287fbb9559869c7e7efd68cf5d4cdcd0
Cris <duqueorange+github@gmail.com>
2023-09-22 00:36:34 -0500

    I actually did it. Single email and password fields. So clean. Next Up: Sign Out Button\Behavior M: Auth status, email, password V: Input fields, buttons. There's a few types of text C: Auth functions, Hide\Show behavior (THIS IS WHY JS IS TO HANDLE THOSE!) However there is a matter of markup

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ const firebaseConfig = {
   appId: "1:934386610035:web:9938d0cd220d303b885f06"
 };
 
-
 // IMPORT FIREBASE, INIT APP AND SERVICES
-// Firebase itself (+ web?)
+  // Firebase itself (+ web?)
 import { 
     initializeApp 
 } from 'firebase/app'
 const app = initializeApp(firebaseConfig)
+
 // Firestore, the database. Implementation later on
 import { 
     getFirestore, collection, onSnapshot,
@@ -23,49 +23,99 @@ import {
     // serverTimestamp
  } from 'firebase/firestore'
 const db = getFirestore()
-// Auth. Plus Auth implementation
+  // Auth. Plus Auth implementation
 import { 
-    createUserWithEmailAndPassword,
     getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
 } from 'firebase/auth'
+
 // Signing users (where's acc creation?)
 // initialize
 const auth = getAuth()
-const signupForm = document.querySelector('.signup')
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const email = signupForm.email.value
-    const password = signupForm.password.value
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((cred)=>{
-        // console.log(cred.user)
-        signupForm.reset()
-        })
-        .catch((err)=>{
-            console.log(err.message)
-        })
+
+// General selector for the “Form” and Inputs
+const authForm = document.querySelector('.auth')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const signUpButton = document.querySelector('#signUpButton')
+const signInButton = document.querySelector('#signInButton')
+// const signOutButton = document.querySelector('#signOutButton')
+
+// SignUp
+signUpButton.addEventListener('click', ()=>{
+  const aemail = email.value
+  const apassword = password.value
+  createUserWithEmailAndPassword(auth,aemail,apassword)
+  .then((cred)=>{
+    console.log(cred.user)
+    // RESUME: Hide\Show\Sh
+    document.getElementById("email").remove()
+    document.getElementById("password").remove()
+    document.getElementById("signUpButton").remove()
+    document.getElementById("signInButton").remove()
+  })
+  .catch((err)=>{
+    console.log(err.code + err.message)
+  })
 })
 
-// PLAYGROUND
-//  Firestore implementarion:
+// RESUME HERE
+// Login
+signInButton.addEventListener('click', ()=>{
+  const aemail = email.value
+  const apassword = password.value
+  signInWithEmailAndPassword(auth, aemail, apassword)
+  .then(cred =>{
+    console.log('user logged in:', cred.user)
+    //
+    document.getElementById("email").remove()
+    document.getElementById("password").remove()
+    document.getElementById("signUpButton").remove()
+    document.getElementById("signInButton").remove()
+  })
+})
+
+
+// Logout
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      console.log('user signed out')
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+// subscribing to auth changes
+const unsubAuth = onAuthStateChanged(auth, (user) => {
+    console.log('user status changed:', user)
+  })
+
+//  Firestore implementation:
 //  Load a Firestore collection, make a promise gets collection data
 //  Use arrow function cause?
-const colRef = collection(db, 'books')
-onSnapshot(colRef, (snapshot) => {
-        let books = []
-        snapshot.docs.forEach((doc) => {
-            books.push({ ...doc.data(), id: doc.id })
+// // const colRef = collection(db, 'books')
+// // onSnapshot(colRef, (snapshot) => {
+// //         let books = []
+// //         snapshot.docs.forEach((doc) => {
+// //             books.push({ ...doc.data(), id: doc.id })
 
-            const h2Element = document.createElement("h2")
-            h2Element.setAttribute('id', doc.id)
-            h2Element.setAttribute('contenteditable', "true")
+// //             const h2Element = document.createElement("h2")
+// //             h2Element.setAttribute('id', doc.id)
+// //             h2Element.setAttribute('contenteditable', "true")
             
-            h2Element.innerText = books[books.length -1].author+", "+books[books.length -1].title
-            document.body.appendChild(h2Element)
-        })
-        console.log(books)
-    })
-    // Standard error catcher
-    .catch(err => {
-        console.log(err.message)
-    })
+// //             h2Element.innerText = books[books.length -1].author+", "+books[books.length -1].title
+// //             document.body.appendChild(h2Element)
+// //         })
+// //         console.log(books)
+// //     })
+// //     // Standard error catcher
+// //     .catch(err => {
+// //         console.log(err.message)
+// //     })
+
